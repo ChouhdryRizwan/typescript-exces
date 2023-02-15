a273e2ce2cc34342001796fc3cf934256e37ab11
Chouhdry-Rizwan <muhammadrizwantahir23@gmail.com>
2023-02-16 00:41:13 +0500

    Word Counter App

---
+++ riz_WordCounterApp/index.js
@@ -0,0 +1,38 @@
+#!usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+// Stop the welcome title
+const sleep = () => new Promise((res, rej) => setTimeout(res, 1000));
+// Create the welcome title
+async function welcome_title() {
+    console.clear();
+    const title = chalkAnimation.rainbow("  ___ Welcome to Word Counter App ___\n");
+    await sleep();
+    title.stop();
+}
+await welcome_title();
+async function wordCounter() {
+    const ans = await inquirer.prompt([
+        {
+            type: "input",
+            name: "paragraph",
+            message: chalk.blueBright("Please enter your Sentence to count the Word."),
+        }
+    ]);
+    const words = ans.paragraph.trim().split(" ");
+    console.log(chalk.green(`Your Sentence Word Count is : ${words.length} .`));
+}
+async function do_again() {
+    do {
+        await wordCounter();
+        var again = await inquirer
+            .prompt({
+            type: "input",
+            name: "restart",
+            message: "\n Do you want to do anotner Convertion? Press 'y' or 'n' : "
+        });
+    } while (again.restart == 'y' || again.restart == 'Y' || again.restart == 'yes' || again.restart == 'YES' || again.restart == 'Yes');
+    console.log(chalk.cyan(`\n ---- Thank you for using this App ----`));
+}
+do_again();
